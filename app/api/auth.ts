134550c2864bc99d18d381bc06489cf5b5a9aef5
Endless <934393489@qq.com>
2023-05-21 22:16:36 +0800

    update REQUEST_LIMIT

--- app/api/auth.ts
+++ app/api/auth.ts
@@ -2,9 +2,8 @@ import { NextRequest } from "next/server";
 import { getServerSideConfig } from "../config/server";
 import md5 from "spark-md5";
 import { ACCESS_CODE_PREFIX } from "../constant";
-import { OPENAI_URL } from "./common";
 
-const REQUEST_LIMIT = 10;
+const REQUEST_LIMIT = 15;
 const REQUEST_MAX_LIMIT = 30;
 const REQUEST_LIMIT_TIME = 60 * 60 * 1000; // 将时间转换为毫秒
 const ipRequests = new Map<string, { count: number; lastTimestamp: number }>();
@@ -68,15 +67,16 @@ export function auth(req: NextRequest) {
   requestRecord.lastTimestamp = currentTimestamp;
 
   // 如果计数器超过了请求数限制，则返回 429 Too Many Requests 响应
-  console.log(requestRecord.count > REQUEST_LIMIT);
-
   // 如果需要 Access Code 并且用户没有提供 Access Code 或提供的 Access Code 不在允许列表中，则返回错误响应
   if (serverConfig.needCode && !serverConfig.codes.has(hashedCode) && !token) {
     if (requestRecord.count > REQUEST_LIMIT) {
       return {
         error: true,
         needAccessCode: true,
-        msg: "当前为免费通道，暂时提供每小时10次/ip免费体验次数。继续使用请以访问密码解锁至每小时30次/ip，请前往设置页面填写您的 Access Code 或 OpenAI API Key",
+        msg:
+          "当前为免费通道，暂时提供每小时" +
+          REQUEST_LIMIT +
+          "次/ip免费体验次数。继续使用请以访问密码解锁至每小时30次/ip，请前往设置页面填写您的 Access Code 或 OpenAI API Key",
         status: 401,
       };
     }
@@ -85,7 +85,10 @@ export function auth(req: NextRequest) {
       return {
         error: true,
         needAccessCode: true,
-        msg: "当前为免费通道，暂时提供最高每小时30次/ip免费请求次数。休息一下再来吧~。 若要继续使用 请前往设置页面填写您的 OpenAI API Key",
+        msg:
+          "当前为免费通道，暂时提供最高每小时" +
+          REQUEST_MAX_LIMIT +
+          "次/ip免费请求次数。休息一下再来吧~。 若要继续使用 请前往设置页面填写您的 OpenAI API Key",
         status: 401,
       };
     }
